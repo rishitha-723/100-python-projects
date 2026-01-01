5f5dcd086a49624d6c42c098a9dc84ffd7ff785c
RISHITHA <rralle2006@gmail.com>
2026-01-01 18:47:56 +0530

    Day-1 printing strings and input string from user

--- Day 1/task/solution.py
+++ Day 1/task/solution.py
@@ -1,2 +1,12 @@
 # Write your code below this line 👇
 print("Hello world!")
+#input string
+print("Hello" +input("name")+ "!")
+#length of the string
+a = input("What is your name?")
+print(len(a))
+#band Name Generator
+city = input("USEr city")
+petname = input("pet name")
+print(city +" "+ petname)
+
